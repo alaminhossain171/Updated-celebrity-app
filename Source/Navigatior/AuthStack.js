cb7d7e9efc914ddd041fc346404fc4d58ff0ce46
M. Aktaruzzaman Joti <63288034+aktaruzzamanjoti97@users.noreply.github.com>
2022-07-14 10:37:51 +0600

    live

--- Source/Navigatior/AuthStack.js
+++ Source/Navigatior/AuthStack.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
 import TabRoutes from './TabRoutes';
 import AuthNavigationString from '../Constants/AuthNavigationString';
 import CelebrityAuth from '../Screens/Auth/CelebrityAuth';
 import Flash from '../Screens/Auth/Flash';
-import Qrc from '../Screens/Auth/Qrc'
+import Qrc from '../Screens/Auth/Qrc';
 import Congratulations from './../Screens/Auth/Congratulations';
 import HelloStar from './../Screens/Auth/HelloStar';
 import Loader from './../Screens/Auth/Loader';
@@ -13,34 +14,32 @@ import SignUp from './../Screens/Auth/SignUp';
 import UserInformation from './../Screens/Auth/UserInformation';
 import Login from './../Screens/Auth/Login';
 
+const AuthStack = Stack => {
+  return (
+    <>
+      <Stack.Screen name={AuthNavigationString.LOGIN} component={Login} />
+      <Stack.Screen name={AuthNavigationString.FLASH} component={Flash} />
+      <Stack.Screen name={AuthNavigationString.QRC} component={Qrc} />
+      <Stack.Screen
+        name={AuthNavigationString.USERINFORMATION}
+        component={UserInformation}
+      />
+      <Stack.Screen name={AuthNavigationString.SIGNUP} component={SignUp} />
+      <Stack.Screen name={AuthNavigationString.OTP} component={Otp} />
+      <Stack.Screen
+        name={AuthNavigationString.HELLOSTAR}
+        component={HelloStar}
+      />
+      <Stack.Screen name={AuthNavigationString.LOADER} component={Loader} />
 
+      {/* <Stack.Screen name={AuthNavigationString.CONGRATULATION} component={Congratulations} /> */}
 
+      <Stack.Screen
+        name={AuthNavigationString.STARAUTH}
+        component={CelebrityAuth}
+      />
+    </>
+  );
+};
 
-
-const AuthStack = (Stack) => {
-    return (
-        <>
-
-
-            <Stack.Screen name={AuthNavigationString.LOGIN} component={Login} />
-            <Stack.Screen name={AuthNavigationString.FLASH} component={Flash} />
-            <Stack.Screen name={AuthNavigationString.QRC} component={Qrc} />
-            <Stack.Screen name={AuthNavigationString.USERINFORMATION} component={UserInformation} />
-            <Stack.Screen name={AuthNavigationString.SIGNUP} component={SignUp} />
-            <Stack.Screen name={AuthNavigationString.OTP} component={Otp} />
-            <Stack.Screen name={AuthNavigationString.HELLOSTAR} component={HelloStar} />
-            <Stack.Screen name={AuthNavigationString.LOADER} component={Loader} />
-
-
-
-            {/* <Stack.Screen name={AuthNavigationString.CONGRATULATION} component={Congratulations} /> */}
-
-
-
-            <Stack.Screen name={AuthNavigationString.STARAUTH} component={CelebrityAuth} />
-
-        </>
-    )
-}
-
-export default AuthStack
+export default AuthStack;
